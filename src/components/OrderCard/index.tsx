2a9436cc8c0d416445a0c5e0e3016ebf1614f6a2
Vinicius Duarte <viniciusduarte@outlook.com>
2023-07-03 17:20:48 +0100

    chore: add formatDate and formatartion

--- src/components/OrderCard/index.tsx
+++ src/components/OrderCard/index.tsx
@@ -1,5 +1,6 @@
 import {CalcAmount} from '../../utils/calcAmount'
 import {colors} from '../../utils/colors'
+import {formatDate} from '../../utils/formatDate'
 import {getDescription} from '../../utils/getDescription'
 import {Order} from '../../utils/types'
 import * as S from './styles'
@@ -12,17 +13,28 @@ export const OrderCard = ({order, onPress}: Types) => {
   return (
     <S.Card
       onPress={onPress}
-      style={{borderBottomColor: `${colors.white}`, borderBottomWidth: 1}}>
+      style={{
+        borderBottomColor: `${colors.white}`,
+        borderBottomWidth: 1,
+      }}>
+      {order.status === 'CLOSED' && (
+        <S.ClosedContainer>
+          <S.ClosedText>Fechado</S.ClosedText>
+        </S.ClosedContainer>
+      )}
       <S.Title>
         <S.Table numberOfLines={1}>{`${order.table}`}</S.Table>
       </S.Title>
-      <S.Footer>
+      <S.Footer style={{opacity: order.status === 'CLOSED' ? 0.2 : 1}}>
         <S.DescriptionContainer>
           <S.Description numberOfLines={4}>
             {getDescription(order)}
           </S.Description>
         </S.DescriptionContainer>
-        <S.Price>{`${CalcAmount(order.products)}`}</S.Price>
+        <S.PriceContainer>
+          <S.Date>{formatDate(order.createdAt)}</S.Date>
+          <S.Price>{`${CalcAmount(order.products)}`}</S.Price>
+        </S.PriceContainer>
       </S.Footer>
     </S.Card>
   )
