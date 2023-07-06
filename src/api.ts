a7378d5c2faa4303a1e3843870b7ab6e20405594
Vinicius Duarte <viniciusduarte@outlook.com>
2023-07-06 01:35:17 +0100

    chore: i close the app, i wanna cry

--- src/api.ts
+++ src/api.ts
@@ -5,6 +5,7 @@ const url = 'http://192.168.123.46:3001/'
 export const api = axios.create({
   baseURL: url,
   timeout: 5000,
+
   headers: {
     Authorization: 'beda9f421b4636b096c1fdc7dc6eb24e',
   },
